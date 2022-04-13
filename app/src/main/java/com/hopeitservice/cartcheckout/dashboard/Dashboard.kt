94822ac1b0998657e324230f6de7d0e23f510e78
Vinod Kumar <kvinodchand@gmail.com>
2022-04-13 14:08:16 +0530

    1. Project setup complete 2. Create all package and activity/fragment 3. Added all useful icon

--- app/src/main/java/com/hopeitservice/cartcheckout/dashboard/Dashboard.kt
+++ app/src/main/java/com/hopeitservice/cartcheckout/dashboard/Dashboard.kt
@@ -1,11 +1,12 @@
-package com.hopeitservice.cartcheckout
+package com.hopeitservice.cartcheckout.dashboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hopeitservice.cartcheckout.R
 
-class MainActivity : AppCompatActivity() {
+class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_dashboard)
     }
 }
